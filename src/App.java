7d5fc4c3f6e3ff6446e1d4ae4fb6f1c4f2a14c1e
m-layher86 <148902757+m-layher86@users.noreply.github.com>
2023-10-25 08:26:35 +0000

    Iterating with ArrayStream

--- src/App.java
+++ src/App.java
@@ -1,6 +1,22 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class App {
-  
+
   public static void main(String[] args) {
-    
+    double[] lotteryNums = new double[] { 45, 92, 38, 33, 21 };
+    System.out.println("Ausgabe = " + lotteryNums[1]);
+
+    for (int i = lotteryNums.length - 1; i >= 0; i = i - 2) {
+      System.out.println(lotteryNums[i]);
+    }
+    System.out.println("/n");
+    for (double item : lotteryNums) {
+      System.out.println(item);
+    }
+    System.out.println("/--");
+    Arrays.stream(lotteryNums).forEach(System.out::println);
+
   }
+
 }
